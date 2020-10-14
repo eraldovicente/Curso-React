09b51a257d7eaaac4a375a214bc5e5b350726eed
Eraldo Vicente <eraldovicente2@gmail.com>
2020-10-14 14:06:12 -0300

    correcao

--- hook-app/src/index.js
+++ hook-app/src/index.js
@@ -6,13 +6,13 @@ import ReactDOM from 'react-dom';
 // import { FocusScreen } from './components/04 - useRef/FocusScreen';
 // import { RealExampleRef } from './components/04 - useRef/RealExampleRef';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
-import { MemoHook } from './components/06-memos/MemoHook';
+// import { Memorize } from './components/06-memos/Memorize';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02 - useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
-
 import { MemoHook } from './components/06-memos/MemoHook';
+
 
 
 ReactDOM.render(
