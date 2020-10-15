6d1e0bad3580b22f4c0685b30a89fa7725e1bc2f
Eraldo Vicente <eraldovicente2@gmail.com>
2020-10-14 23:18:51 -0300

    Otimizacion - listado de TODOS

--- hook-app/src/components/08-useReducer/TodoApp.js
+++ hook-app/src/components/08-useReducer/TodoApp.js
@@ -3,6 +3,7 @@ import { todoReducer } from './todoReducer';
 import { useForm } from '../../hooks/useForm';
 
 import './styles.css';
+import { TodoList } from './TodoList';
 
 const init = () => {
 
@@ -80,29 +81,11 @@ export const TodoApp = () => {
                <div className="row">
 
                     <div className="col-7">
-                         <ul className="list-group list-group-flush">
-                              {
-                                   todos.map( (todo, i) => (
-                                        <li
-                                             key={ todo.id }
-                                             className="list-group-item"
-                                        > 
-                                             <p 
-                                                  className={ `${ todo.done && 'complete' }` }
-                                                  onClick={ () => handleToggle( todo.id ) }     
-                                             > 
-                                                  { i + 1 }. { todo.desc } 
-                                             </p>
-                                             <button
-                                                  className="btn btn-danger"
-                                                  onClick={ () => handleDelete( todo.id ) }
-                                             >
-                                                 Borrar
-                                             </button>
-                                        </li>
-                                   ))
-                              }
-                         </ul>
+                         <TodoList
+                              todos={ todos }
+                              handleDelete={ handleDelete }
+                              handleToggle={ handleToggle }
+                         />
                     </div>
 
                     <div className="col-5">
