1a80fe47d2358f269f2a637d40306d4a0e802496
Eraldo Vicente <eraldovicente2@gmail.com>
2020-10-16 00:14:51 -0300

    useContext

--- hook-app/src/components/09-useContext/MainApp.js
+++ hook-app/src/components/09-useContext/MainApp.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppRouter } from './AppRouter'
 import { UserContext } from './UserContext'
 
 export const MainApp = () => {
 
-     const user = {
-          id: 1234,
-          name: 'Fernando',
-          email: 'fernando.herrera@gmail.com'
-     }
+     const [user, setUser] = useState({});
+
+     // const user = {
+     //      id: 1234,
+     //      name: 'Fernando',
+     //      email: 'fernando.herrera@gmail.com'
+     // }
 
      return (
-          <UserContext.Provider value={ user }>
+          <UserContext.Provider value={{
+               user,
+               setUser
+          }}>
                <AppRouter />
           </UserContext.Provider>
      )
