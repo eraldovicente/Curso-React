5d737ea7fb6cc2ec88a192e082b9dd789628c659
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-02 17:35:53 -0200

    Estilos del authrouter

--- 05-journal-app/src/routers/AuthRouter.js
+++ 05-journal-app/src/routers/AuthRouter.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { RegisterScreen } from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
      return (
-          <div>
-               <Switch>
-                    <Route
-                         exact
-                         path="/auth/Login"
-                         component={ LoginScreen }
-                    />
+          <div className="auth__main">
+               <div className="auth__box-container">
+                    <Switch>
+                         <Route
+                              exact
+                              path="/auth/Login"
+                              component={ LoginScreen }
+                         />
 
-                    <Route
-                         exact
-                         path="/auth/login"
-                         component={ RegisterScreen }
-                    />
+                         <Route
+                              exact
+                              path="/auth/register"
+                              component={ RegisterScreen }
+                         />
 
-                    <Redirect to="auth/login" />
-               </Switch>
+                         <Redirect to="auth/login" />
+                    </Switch>
+               </div>
           </div>
      )
 }
