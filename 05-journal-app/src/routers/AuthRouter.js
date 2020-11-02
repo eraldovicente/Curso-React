d00b6f1ed472358f9d0359041a9d79737a3a68ad
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-02 00:19:16 -0200

    Implementando rutas principales y rutas hijas

---
+++ 05-journal-app/src/routers/AuthRouter.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+
+export const AuthRouter = () => {
+     return (
+          <div>
+               <Switch>
+                    <Route
+                         exact
+                         path="/auth/Login"
+                         component={ LoginScreen }
+                    />
+
+                    <Route
+                         exact
+                         path="/auth/login"
+                         component={ RegisterScreen }
+                    />
+
+                    <Redirect to="auth/login" />
+               </Switch>
+          </div>
+     )
+}
