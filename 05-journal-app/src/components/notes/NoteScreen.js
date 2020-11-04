2e3204b6a0248563b361a593606473fb6f89b3ac
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-04 04:02:53 -0200

    Estructura y deseño del HomeScreen

--- 05-journal-app/src/components/notes/NoteScreen.js
+++ 05-journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,24 @@ export const NoteScreen = () => {
      return (
           <div className="notes__main-content">
                <NotesAppBar />
+               <div className="notes__content">
+                    <input 
+                         type="text"
+                         placeholder="Some Awsome title"
+                         className="notes__title-input"
+                         autoComplete="off"
+                    />
+                    <textarea
+                         placeholder="What happened today"
+                         className="notes__textarea"
+                    />
+                    <div className="notes__image">
+                         <img
+                              src="https://i.pinimg.com/originals/08/28/e8/0828e864c48a2fb94c6977029b16115c.jpg"
+                              alt="imagem"
+                         />
+                    </div>
+               </div>
           </div>
      )
 }
