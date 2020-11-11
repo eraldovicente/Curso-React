874d0e4e86d08faecdc8c4a5285b7651f266c346
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-10 22:43:10 -0200

    Realizar el login de usuario con correo y conf

--- 05-journal-app/src/components/auth/LoginScreen.js
+++ 05-journal-app/src/components/auth/LoginScreen.js
@@ -10,7 +10,7 @@ export const LoginScreen = () => {
      const dispatch = useDispatch();
 
      const [ formValues, handleInputChange ] = useForm({
-          email: 'junior@gmail.com',
+          email: 'eraldo@gmail.com',
           password: '123456'
      });
 
