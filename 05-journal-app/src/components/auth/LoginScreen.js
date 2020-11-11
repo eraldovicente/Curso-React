3183e1c5aa08e101e010628bc22686a52b540697
Eraldo Vicente <eraldovicente2@gmail.com>
2020-11-11 00:58:26 -0200

    Tarea Loading state

--- 05-journal-app/src/components/auth/LoginScreen.js
+++ 05-journal-app/src/components/auth/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
@@ -8,6 +8,7 @@ import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
 export const LoginScreen = () => {
 
      const dispatch = useDispatch();
+     const { loading } = useSelector( state => state.ui );
 
      const [ formValues, handleInputChange ] = useForm({
           email: 'eraldo@gmail.com',
@@ -52,6 +53,7 @@ export const LoginScreen = () => {
                     <button
                          type="submit"
                          className="btn btn-primary btn-block"
+                         disabled={ loading }
                     >
                          Login
                     </button>
