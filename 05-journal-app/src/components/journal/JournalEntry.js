7ac3c764ebfd2d046735bc55786be6c33be79196
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-05 18:15:12 -0300

    Mostrar las notas en el menu lateral

--- 05-journal-app/src/components/journal/JournalEntry.js
+++ 05-journal-app/src/components/journal/JournalEntry.js
@@ -1,26 +1,34 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+
+     const noteDate = moment(date);
+
      return (
           <div className="journal__entry pointer">
-               <div 
-                    className="journal__entry-picture"
-                    style={{
-                         backgroundSize: 'cover',
-                         backgroundImage: 'url(https://i.pinimg.com/originals/80/48/ec/8048eccbb8480be27d14c0ac8dc2468b.jpg)'
-                    }}
-               ></div>
+               {
+                    url &&
+                    <div 
+                         className="journal__entry-picture"
+                         style={{
+                              backgroundSize: 'cover',
+                              backgroundImage: `url(${ url })`
+                         }}
+                    ></div>
+               }
+
                <div className="journal__entry-body">
                     <p className="journal__entry-title">
-                         Un nuevo dia
+                         { title }
                     </p>
                     <p className="journal__entry-content">
-                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         { body }
                     </p>
                </div>
                <div className="journal__entry-date-box">
-                    <span>Monday</span>
-                    <h4>28</h4>
+                    <span>{ noteDate.format('dddd') }</span>
+                    <h4>{ noteDate.format('Do') }</h4>
                </div>
           </div>
      )
