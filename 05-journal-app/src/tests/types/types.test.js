59bf8bc7398bbf7740cdf8ea3c4b6633f015ff05
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-07 19:58:33 -0300

    Continuacion del proyecto - JournalApp - Testing

---
+++ 05-journal-app/src/tests/types/types.test.js
@@ -0,0 +1,27 @@
+import { types } from "../../types/types";
+
+describe('Pruebas con nuestros tipos', () => {
+     
+     test('debe de tener estos tipos', () => {
+          
+          expect( types ).toEqual({
+               ogin: '[Auth] Login',
+               logout: '[Auth] Logout',
+
+               uiSetError: '[UI] Set Error',
+               uiRemoveError: '[UI] Remove Error',
+
+               uiStartLoading: '[UI] Start loading',
+               uiFinishLoading: '[UI] Finish loading',
+
+               notesAddNew: '[Notes] New note',
+               notesActive: '[Notes] Set active note',
+               notesLoad: '[Notes] Load notes',
+               notesUpdated: '[Notes] Updated note',
+               notesFileURL: '[Notes] Updated image url',
+               notesDelete: '[Notes] Delete note',
+               notesLogoutCleaning: '[Notes] Logout Cleaning'
+          })
+     })
+     
+})
