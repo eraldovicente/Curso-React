22bc16cd5f8c523f527995cad6c1803533e36972
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-11 17:35:49 -0300

    Crear base de datos de desarrollo

--- 05-journal-app/src/firebase/firebase-config.js
+++ 05-journal-app/src/firebase/firebase-config.js
@@ -12,7 +12,23 @@ const firebaseConfig = {
      appId: "1:991501244942:web:e3ad937cfc0ea6e38ffa5f"
    };
 
-   firebase.initializeApp(firebaseConfig);
+const firebaseConfigTesting = {
+     apiKey: "AIzaSyDSvS6yqydTyW2wvXG8cfllLG2EHLWhi60",
+     authDomain: "teste-app-396af.firebaseapp.com",
+     projectId: "teste-app-396af",
+     storageBucket: "teste-app-396af.appspot.com",
+     messagingSenderId: "910420210233",
+     appId: "1:910420210233:web:468db07a3f78f8298313e8"
+   };
+
+   if( process.env.NODE_ENV === 'test' ) {
+        // testing
+        firebase.initializeApp(firebaseConfigTesting);
+   } else {
+        // dev/prod
+        firebase.initializeApp(firebaseConfig);
+   }
+
 
    const db = firebase.firestore();
    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
