c75619c8016cc86edbcbfe8850044b4f9d4c1153
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-11 18:20:41 -0300

    Variables de entorno

--- 05-journal-app/src/firebase/firebase-config.js
+++ 05-journal-app/src/firebase/firebase-config.js
@@ -2,32 +2,36 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+// console.log(process.env);
+
 const firebaseConfig = {
-     apiKey: "AIzaSyCykFEdKpAG4d62b4b8QT0u39ElWeTljX8",
-     authDomain: "react-app-cursos-dfec9.firebaseapp.com",
-     databaseURL: "https://react-app-cursos-dfec9.firebaseio.com",
-     projectId: "react-app-cursos-dfec9",
-     storageBucket: "react-app-cursos-dfec9.appspot.com",
-     messagingSenderId: "991501244942",
-     appId: "1:991501244942:web:e3ad937cfc0ea6e38ffa5f"
+     apiKey: process.env.REACT_APP_APIKEY,
+     authDomain: process.env.REACT_APP_AUTHDOMAIN,
+     databaseURL: process.env.REACT_APP_DATABASEURL,
+     projectId: process.env.REACT_APP_PROJECTID,
+     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+     appId: process.env.REACT_APP_APPID
    };
 
-const firebaseConfigTesting = {
-     apiKey: "AIzaSyDSvS6yqydTyW2wvXG8cfllLG2EHLWhi60",
-     authDomain: "teste-app-396af.firebaseapp.com",
-     projectId: "teste-app-396af",
-     storageBucket: "teste-app-396af.appspot.com",
-     messagingSenderId: "910420210233",
-     appId: "1:910420210233:web:468db07a3f78f8298313e8"
-   };
+// const firebaseConfigTesting = {
+//      apiKey: "AIzaSyDSvS6yqydTyW2wvXG8cfllLG2EHLWhi60",
+//      authDomain: "teste-app-396af.firebaseapp.com",
+//      projectId: "teste-app-396af",
+//      storageBucket: "teste-app-396af.appspot.com",
+//      messagingSenderId: "910420210233",
+//      appId: "1:910420210233:web:468db07a3f78f8298313e8"
+//    };
 
-   if( process.env.NODE_ENV === 'test' ) {
-        // testing
-        firebase.initializeApp(firebaseConfigTesting);
-   } else {
-        // dev/prod
-        firebase.initializeApp(firebaseConfig);
-   }
+//    if( process.env.NODE_ENV === 'test' ) {
+//         // testing
+//         firebase.initializeApp(firebaseConfigTesting);
+//    } else {
+//         // dev/prod
+//         firebase.initializeApp(firebaseConfig);
+//    }
+
+   firebase.initializeApp(firebaseConfig);
 
 
    const db = firebase.firestore();
