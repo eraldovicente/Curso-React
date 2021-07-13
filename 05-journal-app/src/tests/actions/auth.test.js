33393558f1bf28ff5b8a03e1a1ca24237b4eadb7
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-13 20:36:02 -0300

    Pruebas en las acciones de Auth

---
+++ 05-journal-app/src/tests/actions/auth.test.js
@@ -0,0 +1,28 @@
+import { login, logout } from "../../actions/auth"
+import { types } from "../../types/types";
+
+describe('Pruebas con las acciones de Auth', () => {
+     
+     test('login y logout deben de crear la acción respectiva', () => {
+          
+          const uid = 'ABC123';
+          const displayName = 'Fernando';
+
+          const loginAction = login( uid, displayName ); 
+          const logoutAction = logout(); 
+
+          expect( loginAction ).toEqual({
+               type: types.login,
+               payload: {
+                    uid,
+                    displayName
+               }
+          });
+
+          expect( logoutAction ).toEqual({
+               type: types.logout
+          });
+          
+     })
+     
+})
