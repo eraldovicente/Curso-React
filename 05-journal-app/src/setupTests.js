25d7a174eb66a659a0a7a6a5e10741a44af14fed
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-14 20:47:22 -0300

    Pruebas en el Sidebar

--- 05-journal-app/src/setupTests.js
+++ 05-journal-app/src/setupTests.js
@@ -1,6 +1,12 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import {createSerializer} from 'enzyme-to-json';
+import Swal from 'sweetalert2';
 
 expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
 Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('sweetalert2', () => ({
+     fire: jest.fn(),
+     close: jest.fn()
+}));
