9fd2a55f628064c97cc926b8ec6ef02f069dcbbb
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-17 20:40:23 -0300

    Custom Middlewares

--- 07-calendar-backend/routes/auth.js
+++ 07-calendar-backend/routes/auth.js
@@ -4,19 +4,21 @@
 */
 
 const { Router } = require('express');
-const { check } = require('express-validator');
 const router = Router();
+const { check } = require('express-validator');
 
 const { crearUsuario, 
      loginUsuario, 
      revalidarToken } = require('../controllers/auth');
-
-router.post(
-     '/new', 
+     const { validarCampos } = require('../middlewares/validar-campos');
+     
+     router.post(
+          '/new', 
      [ // middlewares
           check('name', 'El nombre es obligatorio').not().isEmpty(),
           check('email', 'El email es obligatorio').isEmail(),
-          check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+          check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+          validarCampos
      ],
      crearUsuario 
 );
@@ -25,7 +27,8 @@ router.post(
      '/',
      [
           check('email', 'El email es obligatorio').isEmail(),
-          check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+          check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+          validarCampos
      ], 
      loginUsuario 
 );
