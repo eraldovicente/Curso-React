49060808167ff16722fecf018dfac2e601849717
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-18 22:54:57 -0300

    Modelo Evento

--- 07-calendar-backend/controllers/events.js
+++ 07-calendar-backend/controllers/events.js
@@ -8,6 +8,10 @@ const getEventos = ( req, res = response ) => {
 }
 
 const crearEvento = ( req, res = response ) => {
+
+     // verificar que tenga el evento
+     console.log( req.body );
+
      res.json({
           ok: true,
           msg: 'crearEvento'
