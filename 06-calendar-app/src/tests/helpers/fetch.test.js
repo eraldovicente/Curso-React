f238ba5a3c9c36966074ff5c4699de9997c44b29
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-21 18:02:14 -0300

    Pruebas en nuestro helper del Fetch

---
+++ 06-calendar-app/src/tests/helpers/fetch.test.js
@@ -0,0 +1,17 @@
+
+import { fetchSinToken } from '../../helpers/fetch'
+
+describe('Pruebas en el helper Fetch', () => {
+     
+     test('fetchSinToken debe de funcionar', async() => {
+          
+          const resp = await fetchSinToken('auth', { email: 'Fernando@gmail.com', password: '123456' }, 'POST');
+
+          expect( resp instanceof Response ).toBe( true );
+
+          const body = await resp.json();
+          expect( body.ok ).toBe( true );
+
+     })  
+
+})
