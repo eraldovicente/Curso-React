76b8f74af4bbb30532d58ee002a6e0f5fc9d4ab8
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-21 14:10:14 -0300

    Eliminar un evento

--- 06-calendar-app/src/actions/events.js
+++ 06-calendar-app/src/actions/events.js
@@ -70,7 +70,28 @@ const eventUpdated = ( event ) => ({
      payload: event
 });
 
-export const eventDeleted = () => ({ type: types.eventDeleted });
+export const eventStartDelete = () => {
+     return async ( dispatch, getState ) => {
+
+          const { id } = getState().calendar.activeEvent;
+
+          try {
+               const resp = await fetchConToken(`events/${ id }`, {}, 'DELETE');
+               const body = await resp.json();
+
+               if ( body.ok ) {
+                    dispatch( eventDeleted() );
+               } else {
+                    Swal.fire('Error', body.msg, 'error');
+               }
+
+          } catch (error) {
+               console.log(error);
+          }
+     }
+}
+
+const eventDeleted = () => ({ type: types.eventDeleted });
 
 export const eventStartLoading = () => {
      return async(dispatch) => {
@@ -93,3 +114,5 @@ const eventLoaded = (events) => ({
      type: types.eventLoaded,
      payload: events
 })
+
+export const eventLogout = () => ({ type: types.eventLogout });
--- 06-calendar-app/src/types/types.js
+++ 06-calendar-app/src/types/types.js
@@ -6,6 +6,7 @@ export const types = {
      uiCloseModal: '[ui] Close modal',
 
      eventSetActive: '[event] Set Active',
+     eventLogout: '[event] Logout event',
 
      eventStartAddNew: '[event] Start add new',
      eventAddNew: '[event] Add new',
