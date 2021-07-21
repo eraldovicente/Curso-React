5236142637ca7f67ef71a2905fef701d83351a7d
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-21 17:13:32 -0300

    Inicio de las pruebas

---
+++ 06-calendar-app/src/tests/types/types.test.js
@@ -0,0 +1,32 @@
+import { types } from '../../types/types';
+
+
+describe('Pruebas en Types', () => {
+     
+     test('los types deben de ser iguales', () => {
+          
+          expect( types ).toEqual({
+               
+               uiOpenModal: '[ui] Open modal',
+               uiCloseModal: '[ui] Close modal',
+
+               eventSetActive: '[event] Set Active',
+               eventLogout: '[event] Logout event',
+
+               eventStartAddNew: '[event] Start add new',
+               eventAddNew: '[event] Add new',
+               eventClearActiveEvent: '[event] Clear active event',
+               eventUpdate: '[event] Event updated',
+               eventDeleted: '[event] Event deleted',
+               eventLoaded: '[event] Events loaded',
+
+               authCheckingFinish: '[auth] Finish checking login state',
+               authStartLoging: '[auth] Start login',
+               authLogin: '[auth] Login',
+               authStartRegister: '[auth] Start Register',
+               authStartStartTokenRenew: '[auth] Start token renew',
+               authLogout: '[auth] Logout'
+          })
+     })
+     
+})
