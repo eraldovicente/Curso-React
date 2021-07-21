fb01fc69537f7a6ac33e289e363005559c422044
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-21 18:33:34 -0300

    Pruebas con el helper FetchConToken

--- 06-calendar-app/src/tests/helpers/fetch.test.js
+++ 06-calendar-app/src/tests/helpers/fetch.test.js
@@ -1,9 +1,12 @@
 
-import { fetchSinToken } from '../../helpers/fetch'
+import { fetchConToken, fetchSinToken } from '../../helpers/fetch'
 
 describe('Pruebas en el helper Fetch', () => {
      
+     let token = '';
+
      test('fetchSinToken debe de funcionar', async() => {
+
           
           const resp = await fetchSinToken('auth', { email: 'Fernando@gmail.com', password: '123456' }, 'POST');
 
@@ -12,6 +15,19 @@ describe('Pruebas en el helper Fetch', () => {
           const body = await resp.json();
           expect( body.ok ).toBe( true );
 
+          token = body.token;
+
      })  
+
+
+     test('fetchConToken debe de funcionar', async() => {
+
+          localStorage.setItem('token', token );
+
+          const resp = await fetchConToken('events/60f77e18fc32bb1ce46d2527', {}, 'DELETE');
+          const body = await resp.json();
+
+          expect( body.msg ).toBe('Evento no existe por ese id');
+     })
 
 })
