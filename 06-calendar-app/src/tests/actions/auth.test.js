e6ea3dbfed689c41589cf8993c8894de41fe6354
Eraldo Vicente <eraldovicente2@gmail.com>
2021-07-23 18:08:35 -0300

    Pruebas con un login incorrecto

--- 06-calendar-app/src/tests/actions/auth.test.js
+++ 06-calendar-app/src/tests/actions/auth.test.js
@@ -1,9 +1,14 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import Swal from 'sweetalert2';
 
 import '@testing-library/jest-dom';
 import { startLogin } from '../../actions/auth';
 import { types } from '../../types/types';
+
+jest.mock('sweetalert2', ()=> ({
+     fire: jest.fn()
+}));
 
 const middlewares = [ thunk ];
 const mockStore = configureStore( middlewares );
@@ -23,7 +28,6 @@ describe('Pruebas en las acciones Auth', () => {
      test('startLogin correcto', async() => {
 
          await store.dispatch( startLogin('biana@gmail.com', '123456') );
-
          const actions = store.getActions();
 
          expect( actions[0] ).toEqual({
@@ -39,7 +43,22 @@ describe('Pruebas en las acciones Auth', () => {
 
      //     token = localStorage.setItem.mock.calls[0][1]
      //     console.log(localStorage.setItem.mock.calls[0][1]);
+
+     });
+
+     test('startLogin incorrecto', async() => {
+          
+          await store.dispatch( startLogin('biana@gmail.com', '1234560') );
+          let actions = store.getActions();
+
+          expect(actions).toEqual([]);
+          expect( Swal.fire ).toHaveBeenCalledWith("Error", "Password incorrecto", "error");
+
+          await store.dispatch( startLogin('biana@gmail2.com', '123456') );
+          actions = store.getActions();
+
+          expect( Swal.fire ).toHaveBeenCalledWith("Error", "El usuario no existe con ese email", "error");
+     });
      
-     })
 
 })
